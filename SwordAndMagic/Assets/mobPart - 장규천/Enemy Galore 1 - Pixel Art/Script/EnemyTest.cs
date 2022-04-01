2150f414415c2c1fb7ad5eb24598f8469d18d971
20191249 <nanf3302@gmail.com>
2022-04-01 10:24:17 +0900

    Temporary Add from KC
    몬스터 및 타임라인 패키지만 임시추가

---
+++ SwordAndMagic/Assets/mobPart - 장규천/Enemy Galore 1 - Pixel Art/Script/EnemyTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    [SerializeField] private Animator EnemyAnim;
+    public bool isGolem;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        EnemyAnim = GetComponent<Animator>();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EnemyAnim.SetBool("Run", false);
+            Debug.Log("Idle");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EnemyAnim.SetBool("Run", true);
+            Debug.Log("Running");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EnemyAnim.SetBool("Run", false);
+            EnemyAnim.SetTrigger("Hit");
+            Debug.Log("Hit");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            EnemyAnim.SetBool("Run", false);
+            EnemyAnim.SetTrigger("Death");
+            Debug.Log("Death");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            if (!isGolem)
+            {
+                EnemyAnim.SetBool("Run", false);
+                EnemyAnim.SetTrigger("Ability");
+                Debug.Log("Ability");
+            }
+            else
+            {
+                EnemyAnim.SetBool("Run", false);
+                EnemyAnim.SetBool("Ability", true);
+                Debug.Log("Golem Ability");
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            if (!isGolem)
+            {
+                EnemyAnim.SetBool("Run", false);
+                EnemyAnim.SetTrigger("Attack");
+                Debug.Log("Attack");
+            }
+            else
+            {
+                if (EnemyAnim.GetBool("Ability") == true)
+                {
+                    EnemyAnim.SetBool("Run", false);
+                    EnemyAnim.SetTrigger("Attack");
+                    Debug.Log("Golem Attack");
+                }
+                else
+                {
+                    Debug.Log("Ability not active");
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            EnemyAnim.SetBool("Run", false);
+            EnemyAnim.SetTrigger("Attack 2");
+            Debug.Log("Attack 2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            EnemyAnim.SetBool("Run", false);
+            EnemyAnim.SetTrigger("Attack 3");
+            Debug.Log("Attack 3");
+        }
+    }
+    public void GolemEndAbility()
+    {
+        EnemyAnim.SetBool("Ability", false);
+    }
+}
---
+++ SwordAndMagic/Assets/mobPart - 장규천/Script/TimeLine.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLine : MonoBehaviour
+{
+    public GameObject TimeLineCtrler;
+
+    public List<GameObject> MonsterPoolSettingList;
+    public GameObject NowSpawnPool; 
+
+    private bool isNextPool;
+
+    public int poolNum;
+    void Start()
+    {
+        TimeLineCtrler = GameObject.FindGameObjectWithTag("TimeLineController");
+
+        //poolNum = 0; -> 이거 땜시 리스트가 잘 안 돌아감.
+        //이거 주석처리하니깐 리스트 잘됨 왜지?
+        //아니 초기화 해줘도 ㅈㄹ, 안 해 줘도 ㅈㄹ
+        //알다가도 모르겠네 젠장
+
+        isNextPool = false;
+    }
+
+    public void NextPool()
+    {
+        isNextPool = true;
+
+        //Debug.Log("poolNum : " + poolNum);
+        NowSpawnPool = MonsterPoolSettingList[poolNum];
+        //Debug.Log(NowSpawnPool);
+
+        if (isNextPool)
+        {
+            PoolControl();
+            poolNum++;
+            //Debug.Log("poolNum : " + poolNum);
+            isNextPool = false;
+        }
+
+    }
+
+    void PoolControl()
+    {
+        GameObject MonsterPool_ABC = Instantiate(NowSpawnPool, this.transform.position, Quaternion.identity);
+        MonsterPool_ABC.transform.SetParent(this.transform, false);
+    }
+}
